fcf77cc18912a1b3b8d811d09dcc4c9c7aa0e9e5
sumairq <sumairqaisar992@gmail.com>
2023-12-13 16:27:24 +0500

    Implement sign up flow for user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  resources :users
 
   root "static_pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

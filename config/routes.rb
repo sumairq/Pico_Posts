22c79d59ab8995aa4cd5390ab93c91a85698cecf
sumairq <sumairqaisar992@gmail.com>
2023-12-16 19:28:54 +0500

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'signup', to: 'users#new'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
 
   root "static_pages#home"
